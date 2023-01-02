2888e42021f85466cd62fe353fde46a67d05daaa
spookTacularLDN43 <117355850+spookTacularLDN43@users.noreply.github.com>
2023-01-02 12:43:44 +0100

    color-flipper

---
+++ color-flipper/app.js
@@ -0,0 +1,14 @@
+const colors = ["red", "green", "rgb(200, 150, 50)", "#f15025"]; 
+const btn = document.getElementById('btn'); 
+const color = document.querySelector('.color'); 
+
+btn.addEventListener('click', function() {
+    const randomNumber = getRandomNumber(); 
+    document.body.style.backgroundColor = colors[randomNumber]; 
+    color.textContent = colors[randomNumber]; 
+}
+)
+
+function getRandomNumber(){
+return Math.floor(Math.random() * colors.length); 
+}
